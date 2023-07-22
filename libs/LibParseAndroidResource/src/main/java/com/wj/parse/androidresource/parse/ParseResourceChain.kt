48a98d084e8015073ae40cb6a96ed2c035556d7c
wenjing-bonnie <liuwenjing.12@163.com>
2023-07-22 17:46:37 +0800

    refactor

--- libs/LibParseAndroidResource/src/main/java/com/wj/parse/androidresource/parse/ParseResourceChain.kt
+++ libs/LibParseAndroidResource/src/main/java/com/wj/parse/androidresource/parse/ParseResourceChain.kt
@@ -3,23 +3,74 @@ package com.wj.parse.androidresource.parse
 import com.wj.parse.androidresource.entity.stringpool2.ResStringPoolSecondChunk
 import com.wj.parse.androidresource.entity.table1.ResourceTableHeaderFirstChunk
 import com.wj.parse.androidresource.utils.Logger
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.util.zip.ZipFile
 
 /**
  * the whole resource byte array
  */
-class ParseResourceChain(resourceByteArray: ByteArray) {
+class ParseResourceChain() {
 
-    init {
-        /** read [ResourceTableHeaderFirstChunk]*/
-        Logger.debug("....... begin to read first chunk: Table Header ......")
-        val tableHeaderChunk = ResourceTableHeaderFirstChunk(resourceByteArray).apply {
-            Logger.debug(toString())
-        }
-        /** read [ResStringPoolSecondChunk] */
-        Logger.debug("...... begin to read second chunk: String Pool ......")
-        val stringPoolHeader =
-            ResStringPoolSecondChunk(resourceByteArray, tableHeaderChunk.chunkEndOffset).apply {
+    fun startParseResource(resourcePath: String) {
+        val result = getResourceFromInputStream(FileInputStream(File(resourcePath)))
+
+        result?.let { resourceByteArray ->
+
+            /** read [ResourceTableHeaderFirstChunk]*/
+            Logger.debug("....... begin to read first chunk: Table Header ......")
+            val tableHeaderChunk = ResourceTableHeaderFirstChunk(resourceByteArray).apply {
                 Logger.debug(toString())
             }
+            /** read [ResStringPoolSecondChunk] */
+            Logger.debug("...... begin to read second chunk: String Pool ......")
+            val stringPoolHeader =
+                ResStringPoolSecondChunk(resourceByteArray, tableHeaderChunk.chunkEndOffset).apply {
+                    Logger.debug(toString())
+                }
+        } ?: run {
+            Logger.error("The resource data is null, there is something wrong.")
+        }
+    }
+
+    fun getResourceFromInputStream(resourceInputStream: InputStream): ByteArray? {
+        var srcByteArray: ByteArray? = null
+        var inputStream: InputStream? = null
+        var byteOutputStream: ByteArrayOutputStream? = null
+
+        try {
+            inputStream = resourceInputStream
+            byteOutputStream = ByteArrayOutputStream()
+            var buffer = ByteArray(1024)
+            var length = 0
+            while (inputStream.read(buffer).also { length = it } != -1) {
+                byteOutputStream.write(buffer, 0, length)
+            }
+            srcByteArray = byteOutputStream.toByteArray()
+        } catch (e: Exception) {
+
+        } finally {
+            inputStream?.close()
+            byteOutputStream?.close()
+        }
+        return srcByteArray
+    }
+
+    fun getResourceFromApkFile(apkPath: String): ByteArray? {
+        var srcByteArray: ByteArray? = null
+        var apkFile: ZipFile? = null
+        try {
+            apkFile = ZipFile(apkPath)
+            val entry = apkFile.getEntry("resources.arsc")
+            srcByteArray = getResourceFromInputStream(apkFile.getInputStream(entry))
+            apkFile.close()
+        } catch (e: Exception) {
+
+        } finally {
+            apkFile?.close()
+        }
+        return srcByteArray
     }
 }
