39cbe9512402ef244e429c1f55e620149f868b90
liuwenjing <70619381+wenjing-bonnie@users.noreply.github.com>
2023-08-02 13:05:00 +0800

    Update ResTableConfig.kt

--- libs/LibParseAndroidResource/src/main/java/com/wj/parse/androidresource/entity/typespec6/ResTableConfig.kt
+++ libs/LibParseAndroidResource/src/main/java/com/wj/parse/androidresource/entity/typespec6/ResTableConfig.kt
@@ -2,6 +2,7 @@ package com.wj.parse.androidresource.entity.typespec6
 
 /**
  * create by wenjing.liu at 2023/7/31
+ * https://android.googlesource.com/platform/frameworks/base/+/master/libs/androidfw/include/androidfw/ResourceTypes.h#946
  */
 class ResTableConfig {
 }
