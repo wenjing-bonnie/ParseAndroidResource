e39cc89d3ee32b72e876ced0c6e4d3d5a8b48755
liuwenjing <70619381+wenjing-bonnie@users.noreply.github.com>
2023-08-22 13:01:02 +0800

    Update ResTypeSpecSixChunk.kt

--- libs/LibParseAndroidResource/src/main/java/com/wj/parse/androidresource/entity/typespec6/ResTypeSpecSixChunk.kt
+++ libs/LibParseAndroidResource/src/main/java/com/wj/parse/androidresource/entity/typespec6/ResTypeSpecSixChunk.kt
@@ -26,8 +26,7 @@ import kotlin.experimental.and
  *       // The type identifier this chunk is holding.  Type IDs start
  *       // at 1 (corresponding to the value of the type bits in a
  *       // resource identifier).  0 is invalid.
- *     uint8_t id;
-    
+ *     uint8_t id;    
  *     // Must be 0.
  *     uint8_t res0;
  *     // Must be 0.
@@ -48,9 +47,27 @@ class ResTypeSpecSixChunk(
     override val inputResourceByteArray: ByteArray,
     override val startOffset: Int
 ) : ChunkParseOperator {
+    /**
+     *    // The type identifier this chunk is holding.  Type IDs start
+     *    // at 1 (corresponding to the value of the type bits in a
+     *    // resource identifier).  0 is invalid.
+     *    uint8_t id;  
+     */
     var id: Int = -1
+    /**
+     *     // Must be 0.
+     *     uint8_t res0;
+     */
     var res0: Byte = -1
+    /**
+     *     // Must be 0.
+     *     uint16_t res1; 
+     */
     var res1: Short = -1
+    /**
+     *     // Number of uint32_t entry configuration masks that follow.
+     *     uint32_t entryCount;
+     */
     var entryCount: Int = -1
 
     override val header: ResChunkHeader
@@ -89,12 +106,26 @@ class ResTypeSpecSixChunk(
 
 
     override fun toString(): String =
-        "Part6: ->Type spec header is ${header}\n" +
-                "          id is $id, res0 is $res0, res1 is $res1,  entryCount is $entryCount \n" +
-                "\nPart6: -> End..."
+        formatToString(
+            part = 6,
+            // TODO this index should be set from ResTypeSpecAndTypeInfoSixChunk    
+            childPart = 0,
+            chunkName = "Resource Type spec",
+            "$header",
+            "id is $id, res0 is $res0, res1 is $res1,  entryCount is $entryCount"
+         )
 
     companion object {
+        /**
+         * // Additional flag indicating an entry is public.
+         */
         const val SPEC_PUBLIC = 0x40000000
+        /**
+         * // Additional flag indicating the resource id for this resource may change in a future
+         *  // build. If this flag is set, the SPEC_PUBLIC flag is also set since the resource must be
+         *  // public to be exposed as an API to other applications.
+         */
+        const val SPEC_STAGED_API = 0x20000000u
     }
 
 }
