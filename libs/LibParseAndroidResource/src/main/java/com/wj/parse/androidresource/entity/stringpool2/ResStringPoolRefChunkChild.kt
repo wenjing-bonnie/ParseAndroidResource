aaad17b2c47624fdd64c9d9831fde7eeb3a9f208
liuwenjing <70619381+wenjing-bonnie@users.noreply.github.com>
2023-08-25 16:24:27 +0800

    Update ResStringPoolRefChunkChild.kt

--- libs/LibParseAndroidResource/src/main/java/com/wj/parse/androidresource/entity/stringpool2/ResStringPoolRefChunkChild.kt
+++ libs/LibParseAndroidResource/src/main/java/com/wj/parse/androidresource/entity/stringpool2/ResStringPoolRefChunkChild.kt
@@ -56,8 +56,25 @@ class ResStringPoolRefChunkChild(
     var stringOffsetList = mutableListOf<ResStringPoolRef>()
     var styleOffsetList = mutableListOf<ResStringPoolRef>()
     var stringList = mutableListOf<String>()
-
-    // TODO styleList?????
+    /**
+     * TODO implement
+     * /**
+     * This structure defines a span of style information associated with
+     * a string in the pool.
+     *  */
+     *  struct ResStringPool_span
+     * {
+     *       enum {
+     *       END = 0xFFFFFFFF
+     *   };
+     *   // This is the name of the span -- that is, the name of the XML
+     * // tag that defined it.  The special value END (0xFFFFFFFF) indicates
+     * // the end of an array of spans.
+     * ResStringPool_ref name;
+     * // The range of characters in the string that this span applies to.
+     * uint32_t firstChar, lastChar;
+     * };
+     */
     var styleList = mutableListOf<String>()
 
 
