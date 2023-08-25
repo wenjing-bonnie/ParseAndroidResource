ad8c059575e0bfaef7d51e349e6290d662d0d9d9
liuwenjing <70619381+wenjing-bonnie@users.noreply.github.com>
2023-08-25 11:19:32 +0800

    Update ResTableTypeMapChunkChild.kt

--- libs/LibParseAndroidResource/src/main/java/com/wj/parse/androidresource/entity/typespec6/ResTableTypeMapChunkChild.kt
+++ libs/LibParseAndroidResource/src/main/java/com/wj/parse/androidresource/entity/typespec6/ResTableTypeMapChunkChild.kt
@@ -62,6 +62,13 @@ class ResTableTypeMapChunkChild(
     )
 
     /**
+     * https://android.googlesource.com/platform/frameworks/base/+/master/libs/androidfw/include/androidfw/ResourceTypes.h#417
+     *  This is a reference to a unique entry (a ResTable_entry structure)
+     *  in a resource table.  The value is structured as: 0xpptteeee,
+     *  where pp is the package index, tt is the type index in that
+     *  package, and eeee is the entry index in that type.  The package
+     *  and type values start at 1 for the first item, to help catch cases
+     *  where they have not been supplied.
      * struct ResTable_ref
      * {
      *    uint32_t ident;
